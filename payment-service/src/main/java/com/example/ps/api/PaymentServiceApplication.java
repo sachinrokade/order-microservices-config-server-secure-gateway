487014ed9822a4a466d8a082fe445ffc7b96a30d
sachin.ruplal.rokade <srokade918@gmail.com>
2022-05-11 10:49:31 +0530

    commit from intellij idea

--- payment-service/src/main/java/com/example/ps/api/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/example/ps/api/PaymentServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
 
